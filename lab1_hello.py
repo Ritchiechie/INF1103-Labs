c8f5a77d2dfafa6ccc8ba718d3b0ed3a1fb5652b
ritchie <ritchiechung15@gmail.com>
2026-09-04 11:05:17 +0800

    Fourth Post

--- lab1_hello.py
+++ lab1_hello.py
@@ -3,20 +3,12 @@ print("Welcome here")
 print("=====================================")
 
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
+username = input("Enter Usrname: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
 
-followers += 50
-print("Day 1:", followers)
-
-followers += 20
-print("Day 2:", followers)
-
-followers += 10
-print("Day 3:", followers)
-
-print("Username: ",username)
-print("Bio: ",bio)
-print("Followers:",followers)
-  
+print("\nInstagram Profile")
+print("=====================================")
+print("Username: ", username)
+print("Age: ", age)
+print("Content Category: ", category)
