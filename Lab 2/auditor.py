a02ab570fe1c45f5f8f046d71a06710b7e0c295d
ritchie <ritchiechung15@gmail.com>
2026-09-11 11:52:18 +0800

    Trigger overstock alert and print inventory when user types quit

--- Lab 2/auditor.py
+++ Lab 2/auditor.py
@@ -4,12 +4,17 @@ while True:
     quantity = input("Enter the quantity of items to add to inventory: ")
     if quantity == "quit":
         print("Exiting program....")
+        print(inventory)
         break
 
     elif quantity.isdigit():
         quantity = int(quantity)
         inventory += quantity
         print(inventory)
+
+        if inventory >= 500:
+            print("Inventory limit reached!!!")
+            break
 
     else: 
         print("Invalid input. Please enter a valid quantity or 'quit' to exit.")
