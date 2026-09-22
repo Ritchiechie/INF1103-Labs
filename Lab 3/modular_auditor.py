673bc8ca37569671cb1b19c815663e7c6d2cd169
ritchie <ritchiechung15@gmail.com>
2026-09-22 15:17:57 +0800

    get_valid-input function improvement

--- Lab 3/modular_auditor.py
+++ Lab 3/modular_auditor.py
@@ -2,28 +2,33 @@ inventory = 0
 failed_entries = 0
 
 def get_valid_input():
-    return input("Enter the quantity of items to add to inventory: ")
+    global inventory
+    #global failed_entries
+
+    quantity = input("Enter the quantity of items to add to inventory: ")
+    
+    if quantity.isdigit():
+        quantity = int(quantity)
+        inventory += quantity
+        return quantity
+    elif quantity == "quit":
+        return "quit"
+    else:
+        print("Invalid input. Please enter a valid quantity or 'quit' to exit.")
+        failed_entries += 1
+        return failed_entries
 
 
 while True:
     quantity = get_valid_input()
 
     if quantity == "quit":
-        print("Exiting program....")
-        print("Total units processed: ", inventory)
-        print("Failed/Rejected entries: ", failed_entries)
-        break
-
-    elif quantity.isdigit():
-        quantity = int(quantity)
-        inventory += quantity
-        if inventory >= 500:
-            print("Inventory limit reached!!!")
-            print(inventory)    
+            print("Exiting program....")
+            print("Total units processed: ", inventory)
+            print("Failed/Rejected entries: ", failed_entries)
             break
 
-    else: 
-        failed_entries += 1
-        print("Invalid input. Please enter a valid quantity or 'quit' to exit.")
+   
+        
         
         
