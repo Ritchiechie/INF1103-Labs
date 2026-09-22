eb98937c4bc57fc06ee0d2398e120f787013b411
ritchie <ritchiechung15@gmail.com>
2026-09-22 21:47:52 +0800

    added process_delivery() and also made adjustments to get_valid_input()

--- Lab 3/modular_auditor.py
+++ Lab 3/modular_auditor.py
@@ -2,31 +2,36 @@ inventory = 0
 failed_entries = 0
 
 def get_valid_input():
-    global inventory
-    #global failed_entries
+    get_input = input("Enter the quantity of items to add to inventory: ")
 
-    quantity = input("Enter the quantity of items to add to inventory: ")
-    
-    if quantity.isdigit():
-        quantity = int(quantity)
-        inventory += quantity
-        return quantity
-    elif quantity == "quit":
+    if get_input.isdigit():
+        return int(get_input)
+   
+    elif get_input == "quit":
         return "quit"
     else:
         print("Invalid input. Please enter a valid quantity or 'quit' to exit.")
-        failed_entries += 1
-        return failed_entries
+        return None
 
+def process_delivery(current_total, new_value):
+    new_total = current_total + new_value
+    print("Inventory updated. Current total:", new_total)
+    return new_total 
 
 while True:
-    quantity = get_valid_input()
-
-    if quantity == "quit":
+    user_input = get_valid_input()
+    
+    if user_input == "quit":
             print("Exiting program....")
             print("Total units processed: ", inventory)
             print("Failed/Rejected entries: ", failed_entries)
             break
+
+    elif user_input is None:
+         failed_entries += 1
+
+    else:
+        inventory = process_delivery(inventory, user_input)
 
    
         
